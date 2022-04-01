154e617500a40296c89a85fc20ed15f93803f525
yoptohlejepeta <peta.kotlan@seznam.cz>
2022-04-01 13:11:04 +0200

    algoritmus

--- Eukliduv_algoritmus.py
+++ Eukliduv_algoritmus.py
@@ -1,2 +1,9 @@
 def GCD(m,n):
-    ...
+    a1 = m
+    a2 = n
+    while a2:
+        a1,a2 = a2, a1%a2
+    return f'Největší společný dělitel čísel {m} a {n} je číslo {a1}. '
+
+print(GCD(3997,2947))
+# Největší společný dělitel čísel 3997 a 2947 je číslo 7.
