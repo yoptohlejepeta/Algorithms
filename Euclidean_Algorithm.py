debc46fff37027b60aa0a530aa5a8b66e6093485
yoptohlejepeta <101563288+yoptohlejepeta@users.noreply.github.com>
2022-04-04 22:01:50 +0200

    improved euclidean algorithm

--- Euclidean_Algorithm.py
+++ Euclidean_Algorithm.py
@@ -8,3 +8,21 @@ def GCD(m,n):
 
 print(GCD(3997,2947))
 # Greatest common divider of numbers 3997 and 2947 is 7.
+
+def GCD2(m,n):
+    a = [m,n]
+    b = [0,1]
+    c = [1,0]
+    iterace = 0
+    while a[1]:
+        b[0], b[1] = b[1], b[0] + b[1]*int(a[0]/a[1])
+        c[0], c[1] = c[1], c[0] + c[1]*int(a[0]/a[1])
+        a[0], a[1] = a[1], a[0]%a[1]
+        iterace += 1
+    if iterace % 2 == 0:
+        return f'Greatest common divider is {a[0]} = {(b[1] - b[0])}x{n} - {(c[1] - c[0])}x{m}'
+    else:
+        return f'Greatest common divider is {a[0]} = {(c[1] - c[0])}x{m} - {(b[1] - b[0])}x{n}'
+
+print(GCD2(3997,2947))
+#Greatest common divider is 7 = 334x3997 - 453x2947
