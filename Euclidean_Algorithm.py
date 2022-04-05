f4b229b43841e84086cde9608419d68f63fcc1f9
yoptohlejepeta <101563288+yoptohlejepeta@users.noreply.github.com>
2022-04-05 19:36:32 +0200

    perfecting eng

--- Euclidean_Algorithm.py
+++ Euclidean_Algorithm.py
@@ -3,11 +3,11 @@ def GCD(m,n):
     a2 = n
     while a2:
         a1,a2 = a2, a1%a2
-    return f'Greatest common divider of numbers {m} and {n} is {a1}. '
+    return f'Greatest common divisor of numbers {m} and {n} is {a1}. '
 
 
 print(GCD(3997,2947))
-# Greatest common divider of numbers 3997 and 2947 is 7.
+# Greatest common divisor of numbers 3997 and 2947 is 7.
 
 def GCD2(m,n):
     a = [m,n]
@@ -20,9 +20,9 @@ def GCD2(m,n):
         a[0], a[1] = a[1], a[0]%a[1]
         iteration += 1
     if iteration % 2 == 0:
-        return f'Greatest common divider is {a[0]} = {(b[1] - b[0])}x{n} - {(c[1] - c[0])}x{m}'
+        return f'Greatest common divisor is {a[0]} = {(b[1] - b[0])}x{n} - {(c[1] - c[0])}x{m}'
     else:
-        return f'Greatest common divider is {a[0]} = {(c[1] - c[0])}x{m} - {(b[1] - b[0])}x{n}'
+        return f'Greatest common divisor is {a[0]} = {(c[1] - c[0])}x{m} - {(b[1] - b[0])}x{n}'
 
-#print(GCD2(3997,2947))
-#Greatest common divider is 7 = 334x3997 - 453x2947
+print(GCD2(3997,2947))
+#Greatest common divisor is 7 = 334x3997 - 453x2947
